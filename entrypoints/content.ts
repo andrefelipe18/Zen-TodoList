317c2733c991ef9f4775e56cdad8e6ab6c9f4403
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-24 22:20:37 -0300

    FIX

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,5 +1,5 @@
 export default defineContentScript({
-  matches: ['*://*.zen-browser.app/*'],
+  matches: [''],
   main() {
     console.log('Hello from content script in Min ToDoList Extension! Thanks for the downloading! 🚀');
   },
